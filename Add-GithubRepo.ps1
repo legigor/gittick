b9e9570aa9ae0511febdd2c6a928f9ba637fa1b2
Igor Tamashchuk <legigor@gmail.com>
2015-01-22 01:04:52 +0200

    Implement authorization token create

--- Add-GithubRepo.ps1
+++ Add-GithubRepo.ps1
@@ -1,14 +1,5 @@
-
 $path = Resolve-Path("~\.gittick")
-
-$enc = [IO.File]::ReadAllText($path)
-$pass = Read-Host -Prompt "Passphrase"
-
-$encData = [Convert]::FromBase64String($enc)
-$passData = [Text.Encoding]::Default.GetBytes($pass)
-
-$tokenData = [Security.Cryptography.ProtectedData]::Unprotect($encData, $passData, "CurrentUser")
-$token = [Text.Encoding]::Default.GetString($tokenData)
+$token = [IO.File]::ReadAllText($path)
 
 $project = Get-Location | Split-Path -Leaf
 $postRepo = @{
@@ -18,8 +9,8 @@ $postRepo = @{
     "has_downloads" = "false"
 }
 
-$response = Invoke-WebRequest -Method Post -Uri ("https://api.github.com/user/repos?access_token=" + $token) -Body (ConvertTo-Json $postRepo)
-$repo = ($response.Content | ConvertFrom-Json).ssh_url
+$response = Invoke-RestMethod -Method Post -Uri ("https://api.github.com/user/repos?access_token=" + $token) -Body (ConvertTo-Json $postRepo)
+$url = $response.ssh_url
 
 git init
-git remote add origin $repo
+git remote add origin $url
--- Set-GithubToken.ps1
+++ Set-GithubToken.ps1
@@ -1,9 +1,43 @@
-$token = Read-Host -Prompt "GitHub Token"
-$pass = Read-Host -Prompt "Passphrase"
+$ghLogin = Read-Host -Prompt "GitHub Login"
 
-$tokenData = [Text.Encoding]::Default.GetBytes($token)
-$passData = [Text.Encoding]::Default.GetBytes($pass)
+$ghPassResponse = Read-Host -Prompt "GitHub Password" -AsSecureString
+$ghPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($ghPassResponse))
 
-$encData = [Security.Cryptography.ProtectedData]::Protect($tokenData, $passData, "CurrentUser")
+$headers = @{
+    "authorization" = "basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $ghLogin, $ghPass)))
+}
 
-[Convert]::ToBase64String($encData) > "~\.gittick"
+function Create-Authorization(){
+
+    # $headers
+    $authorization = (ConvertTo-Json @{
+         "note" = ("gittick for " + [Environment]::UserDomainName + "\" + [Environment]::UserName + "@" + $env:COMPUTERNAME)
+         "scopes" = @("repo")
+    })
+
+    # $authorization
+
+    $response = Invoke-RestMethod "https://api.github.com/authorizations" -me Post -h $headers -b $authorization
+    $token = $response.token
+
+    $token > "~\.gittick"
+}
+
+try {
+
+    Create-Authorization
+}
+catch {
+    $otp = $_.Exception.Response.Headers["X-GitHub-OTP"];
+    if($otp -ne $null -and $otp.Contains("required")){
+        
+        $headers["X-GitHub-OTP"] = Read-Host -Prompt ("Authentication code ")
+        Create-Authorization
+
+
+    } else {
+
+        throw
+
+    }    
+}
