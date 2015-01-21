915d7763bc3f3cadf81e1abda6d299bd974d7826
Igor Tamashchuk <legigor@gmail.com>
2015-01-21 02:44:39 +0200

    Remove redundant request for user info (used it for test purposes)

--- Add-GithubRepo.ps1
+++ Add-GithubRepo.ps1
@@ -10,11 +10,7 @@ $passData = [Text.Encoding]::Default.GetBytes($pass)
 $tokenData = [Security.Cryptography.ProtectedData]::Unprotect($encData, $passData, "CurrentUser")
 $token = [Text.Encoding]::Default.GetString($tokenData)
 
-$response = Invoke-WebRequest ("https://api.github.com/user?access_token=" + $token)
-$user = ($response.Content | ConvertFrom-Json).login
-
 $project = Get-Location | Split-Path -Leaf
-
 $postRepo = @{
     "name" = $project
 }
